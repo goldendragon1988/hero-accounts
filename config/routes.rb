7e457b7c19ead6bb972aafc63ef8445fac5c9c57
jim <jag.arnold.go@gmail.com>
2021-03-20 09:21:24 +0800

    add rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users, controllers: {
+        sessions: 'sessions',
+        registrations: 'registrations',
+      }
+    end
+  end
   root to: "home#ping"
 end
